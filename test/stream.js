1964d59dfb5d6b6be7b9b93d02aea7f1bc5dd028
Thomas Urban <thomas.urban@cepharum.de>
2017-09-25 03:55:39 +0200

    converting test-only SubstringParser into officially featured subclass

--- test/stream.js
+++ test/stream.js
@@ -32,7 +32,7 @@ const { Readable, Writable, Duplex, Transform } = require( "stream" );
 
 const Should = require( "should" );
 
-const { Stream, Parser, Context, BufferStream } = require( "../" );
+const { Stream, Parser, Context, BufferStream, SubstringParser } = require( "../" );
 
 
 suite( "ParsingStream", function() {
@@ -157,50 +157,3 @@ suite( "Piping data through ParsingStream", function() {
 			} );
 	} );
 } );
-
-/**
- *
- */
-class SubstringParser extends Parser {
-	/**
-	 * @param {Buffer} sub
-	 */
-	constructor( sub ) {
-		super();
-
-		this._index = 0;
-		this._sub = sub;
-	}
-
-	/**
-	 * Parses provided buffer for containing data matching expectations.
-	 *
-	 * @param {Context} context reference on context for sharing results of parsing
-	 * @param {Buffer} buffer slice of data to be parsed
-	 * @param {int} atOffset provides offset of provided buffer in context of a larger file or stream
-	 * @returns {Promise<ParserResult>}
-	 */
-	parse( context, buffer, atOffset ) {
-		let matchIndex = this._index;
-
-		for ( let read = 0, length = buffer.length; read < length; read++ ) {
-			if ( buffer[read] === this._sub[matchIndex] ) {
-				matchIndex++;
-
-				if ( matchIndex >= this._sub.length ) {
-					this._index = 0;
-
-					const firstIndex = read - matchIndex + 1;
-
-					return this._fullMatch( context, buffer, atOffset, firstIndex, read );
-				}
-			} else {
-				matchIndex = 0;
-			}
-		}
-
-		this._index = matchIndex;
-
-		return this._partialMatch( buffer, buffer.length - matchIndex );
-	}
-}
